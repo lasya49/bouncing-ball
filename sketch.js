ca25e3638ab1c2ed59c6edc2bf9c485100caf916
lasya49 <71409388+lasya49@users.noreply.github.com>
2020-11-07 21:10:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+var ground;
+var ball;
+function setup() {
+  var canvas=createCanvas(800,400);
+  engine =Engine.create();
+  world = engine.world;
+  var ground_options={
+    isStatic:true 
+  }
+  ground = Bodies.rectangle(400,380,800,50,ground_options);
+  World.add(world,ground);
+  var ball_options={
+    restitution:1.0
+  }
+  
+ball=Bodies.circle(200,100,20,ball_options);
+World.add(world,ball);
+  
+  
+}
+
+function draw() {
+  background(255,255,255);
+  Engine.update(engine); 
+  rectMode(CENTER); 
+  //rect(400,200,30,30);
+  rect(ground.position.x,ground.position.y,800,50);
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,20);
+}
